55c160385e3c0816ca3c4fad241b2a7229b0cd3c
Ziring Tawfique <almanzt@gmail.com>
2019-01-27 16:05:10 +0100

    Project Template

---
+++ Client/PostIt/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PostIt.Models;
+
+namespace PostIt.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
