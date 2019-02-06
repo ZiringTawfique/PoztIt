569fb6813d04f328d8a2bc57b57d3f4234b588fd
Ziring Tawfique <almanzt@gmail.com>
2019-02-06 21:50:15 +0100

    Adding post CRUD

--- Backend/Application/UseCases/PostUseCase/IPostCRUD.cs
+++ Backend/Application/UseCases/PostUseCase/IPostCRUD.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Model;
+
 namespace Application.UseCases.PostUseCase
 {
     public interface IPostCRUD
     {
+        void CreatePost();
+        Task<Post> GetPost(int postId);
+        Task<IReadOnlyCollection<Post>> GetPosts();
+        void UpdatePost(Post post);
+        void DeletePost(int postId);
+
     }
 }
---
+++ Backend/Application/UseCases/PostUseCase/PostCRUD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Model;
+
+namespace Application.UseCases.PostUseCase
+{
+    public class PostCRUD : IPostCRUD
+    {
+        public PostCRUD()
+        {
+        }
+
+        public void CreatePost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<Post>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
